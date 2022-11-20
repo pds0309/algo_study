f175e36e2fc0c1c377eb623c827254d1cd43a218
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-11-20 13:45:20 +0900

    solved: pg/다트게임/N/24
    아이유최고

---
+++ src/programmers/hang/다트게임.js
@@ -0,0 +1,25 @@
+const DART_BONUS = {'S': 1, 'D': 2, 'T': 3};
+
+function solution(dartResult) {
+    const darts = [...dartResult.matchAll(/[0-9]+[SDT][*#]?/g)].map(v => v[0]);
+    let doubledValue = 0;
+    let tempPrev = 0;
+    return darts.reduce((prev, next, arr, arr2) => {
+        const [resp, prevDouble] = dartToScore(next);
+        if(prevDouble) {
+            doubledValue += tempPrev;
+        }
+        tempPrev = resp;
+        return prev + resp;
+    }, 0) + doubledValue;
+}
+
+function dartToScore(dart) {
+    const currentScore = parseInt(dart);
+    const rest = dart.replace(currentScore, "");
+    const result = Math.pow(currentScore, DART_BONUS[rest[0]]);
+    if(!rest[1]) {
+        return [result , false];
+    }
+    return rest[1] === '*' ? [result * 2, true] : [result * -1, false];
+}
