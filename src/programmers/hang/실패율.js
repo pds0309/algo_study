706aaa86fd61ec5a15e27ad0d44c95ecf2855acb
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-11-20 12:47:20 +0900

    solved: pg/실패율/E/15

---
+++ src/programmers/hang/실패율.js
@@ -0,0 +1,8 @@
+function solution(N, stages) {
+    const map = new Map();
+    [...Array(N)].forEach((v,i)=> map.set(i + 1,0));
+    Array.from(map.keys()).forEach((key) => {
+       map.set(key, stages.filter(val => val == key).length  / stages.filter(val => val >= key).length);
+    });
+    return [...map.entries()].sort((o1,o2) => o2[1] - o1[1]).map(v => v[0]);
+}
